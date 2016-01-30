30139e83c70ad7e91012ad35409827d2629b7ed9
Marcin Paszkiewicz <mkpaszkiewicz@gmail.com>
2016-01-30 17:13:19 +0100

    Generater vehicles .sql

---
+++ generators/insert_vehicles.sql
@@ -0,0 +1,57 @@
+-- Insert Autobusy_modele
+
+INSERT INTO `Autobusy_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Czy_przegubowy`,`Pojemnosc_skokowa`)
+VALUES ("Scania","CN94UA",0,0,3500);
+
+INSERT INTO `Autobusy_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Czy_przegubowy`,`Pojemnosc_skokowa`)
+VALUES ("Ikarbus","Ik-105",1,1,5500);
+
+INSERT INTO `Autobusy_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Czy_przegubowy`,`Pojemnosc_skokowa`)
+VALUES ("Solbus","Solcity 11L",1,1,2500);
+
+INSERT INTO `Autobusy_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Czy_przegubowy`,`Pojemnosc_skokowa`)
+VALUES ("Scania","N113CLL",1,0,5500);
+
+INSERT INTO `Autobusy_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Czy_przegubowy`,`Pojemnosc_skokowa`)
+VALUES ("Rocar","DAC 112 UDM",0,1,6000);
+
+INSERT INTO `Autobusy_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Czy_przegubowy`,`Pojemnosc_skokowa`)
+VALUES ("Tata Motors","Marcopolobus",1,0,2500);
+
+INSERT INTO `Autobusy_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Czy_przegubowy`,`Pojemnosc_skokowa`)
+VALUES ("Scania","CN94UB",0,0,5000);
+
+INSERT INTO `Autobusy_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Czy_przegubowy`,`Pojemnosc_skokowa`)
+VALUES ("Tata Motors","Starbus",1,1,6000);
+
+INSERT INTO `Autobusy_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Czy_przegubowy`,`Pojemnosc_skokowa`)
+VALUES ("Solbus","Solcity SM 18 LNG",1,0,5500);
+
+INSERT INTO `Autobusy_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Czy_przegubowy`,`Pojemnosc_skokowa`)
+VALUES ("Ikarbus","Ik-161",1,0,2500);
+
+-- Insert Tramwaje_modele
+
+INSERT INTO `Tramwaje_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Napiecie_zasilania`,`Czy_dwukierunkowy`)
+VALUES ("Modertrans","Alfa HF 07",1,300,0);
+
+INSERT INTO `Tramwaje_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Napiecie_zasilania`,`Czy_dwukierunkowy`)
+VALUES ("Alstom","AGV",1,300,1);
+
+INSERT INTO `Tramwaje_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Napiecie_zasilania`,`Czy_dwukierunkowy`)
+VALUES ("Modertrans","Alfa HF 01",1,140,0);
+
+INSERT INTO `Tramwaje_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Napiecie_zasilania`,`Czy_dwukierunkowy`)
+VALUES ("Pesa","219M Atribo",0,300,1);
+
+INSERT INTO `Tramwaje_modele` (`Marka`,`Model`,`Czy_niskopodlogowy`,`Napiecie_zasilania`,`Czy_dwukierunkowy`)
+VALUES ("Pesa","620M",0,260,1);
+
+-- Insert Metra_modele
+
+INSERT INTO `Metra_modele` (`Producent`,`Model`,`Ilosc_wagonow`,`Dopuszczalna_predkosc`)
+VALUES ("Siemens","Inspiro",5,80);
+
+INSERT INTO `Metra_modele` (`Producent`,`Model`,`Ilosc_wagonow`,`Dopuszczalna_predkosc`)
+VALUES ("Alstom","Urbanpolo",5,75);
+
