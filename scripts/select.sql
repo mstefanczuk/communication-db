bd91ddf6f04e385427da74078859ad71452d7f7f
Marcin Paszkiewicz <mkpaszkiewicz@gmail.com>
2016-01-30 19:21:44 +0100

    Updated scripts

--- scripts/select.sql
+++ scripts/select.sql
@@ -1,3 +1,12 @@
 select * from `Poczty`;
 select * from `Adresy`;
 select * from `Biura`;
+
+select * from `Pojazdy_T`;
+select * from `Autobusy_T`;
+select * from `Tramwaje_T`;
+select * from `Metra_T`;
+
+select * from `Autobusy_modele`;
+select * from `Tramwaje_modele`;
+select * from `Metra_modele`;
